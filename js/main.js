3a85a2938c0b1f4852e3b56c3e20da0c7e79df89
Jaloliddin-Zokirov <jaloliddinzokirov1908@gmail.com>
2022-05-10 22:52:59 +0500

    add js file

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var elButton = document.querySelector('.header__button');
+var elMenu = document.querySelector('.header__menu');
+
+elButton.addEventListener('click', function () {
+	elMenu.classList.toggle('header__menu--open');
+});
+
+
+
+var elButtontwo = document.querySelector('.menu__btn');
+var elMenutwo = document.querySelector('.header__menu');
+
+elButtontwo.addEventListener('click', function () {
+	elMenutwo.classList.toggle('header__menu--open');
+});
