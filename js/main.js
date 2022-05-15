0d6ccc45282cf6ed4047aea4da4c04a35835f815
Jaloliddin-Zokirov <jaloliddinzokirov1908@gmail.com>
2022-05-16 01:21:11 +0500

    Add a page about

--- js/main.js
+++ js/main.js
@@ -1,27 +1,21 @@
-var elButton = document.querySelector('.header__button');
-var elMenu = document.querySelector('.header__menu');
+var elButton = document.querySelector('.header__button')
+var elMenu = document.querySelector('.header__menu')
 
 elButton.addEventListener('click', function () {
-	elMenu.classList.toggle('header__menu--open');
-});
+	elMenu.classList.toggle('header__menu--open')
+})
 
 
-
-var elButtontwo = document.querySelector('.menu__btn');
-var elMenutwo = document.querySelector('.header__menu');
+var elButtontwo = document.querySelector('.menu__btn')
+var elMenutwo = document.querySelector('.header__menu')
 
 elButtontwo.addEventListener('click', function () {
-	elMenutwo.classList.toggle('header__menu--open');
-});
+	elMenutwo.classList.toggle('header__menu--open')
+})
+
 
 var elBody = document.querySelector('body')
 
 elBody.addEventListener("click", function() {
 	elBody.classList.toggle('overflow')
-})
-
-var elPlus = document.querySelector('.item__plus')
-
-elPlus.addEventListener('click', function () {
-	elPlus.classList.toggle('.item__plus--open')
 })
